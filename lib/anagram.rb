4d06f60e394930a8022e46794d5b837372a63d75
Sean <craig.sean92@gmail.com>
2022-04-10 21:55:07 -0600

    commit tree 8aef230ccbd061ecf037808ca6bbefbe83a2ac29

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+require "pry"
+
+class Anagram
+    attr_reader :word
+    
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        
+        array.filter{|ana_word| ana_word.chars.sort == @word.chars.sort}
+        
+    end
+end
+
